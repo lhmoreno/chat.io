5737b1b30a5b9aa6b659dd573f97fa6e47b3c4ca
lhmoreno <lhmorenoti@hotmail.com>
2021-10-22 14:40:07 -0300

    feat: Navigate server

--- public/login/script.js
+++ public/login/script.js
@@ -1,11 +1,49 @@
+const params = new URLSearchParams(window.location.search)
 const url_chat = window.location.origin + "/chat"
 
-window.onload = () => {
-  const input = document.querySelector("input")
-
-  input.onchange = (event) => {
-    const name = event.target.value
-  
-    return window.location.href = url_chat + `?name=${name}`
+/*
+  Message: {
+    type: "GET" | "SEND"
+    text: string
   }
+  Messages: {
+    contact_id: string
+    messages: Message[]
+  }
+*/
+
+window.onload = async () => {
+  const name = params.get("name")
+
+  if (name) {
+    const input = document.querySelector("input")
+    input.value = name
+  }
+
+  // if (user_id) {
+  //   const { status, data } = await axios.post(window.location.origin + "/api/login", { user_id })
+    
+  //   if (status === 200) {
+
+  //   } 
+    
+  //   if (status === 201) {
+  //     localStorage.setItem("user_id", data.user_id)
+  //     localStorage.setItem("messages", JSON.stringify([]))
+  //   }
+
+  //   // return navigateChat()
+  // } else {
+  //   const input = document.querySelector("input")
+  
+  //   input.onchange = (event) => {
+  //     const name = event.target.value
+    
+  //     // return navigateChat()
+  //   }
+  // }
+}
+
+function navigateChat() {
+  return window.location.href = url_chat
 }
