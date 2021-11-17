91fb9982e517c1ae6a02353ad29880eef2e369ea
lhmoreno <lhmorenoti@hotmail.com>
2021-11-17 16:47:55 -0300

    refactor: Create session

--- server/src/controllers/userController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express'
-
-export const userController = {
-  async show(req: Request, res: Response) {
-    // const token = req.headers.authorization
-
-    return res.status(200).json({ name: 'lhmoreno' })
-  }
-}
