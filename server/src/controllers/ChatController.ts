0fa025ba1c965bace69b94edadcf86b5aa3e64d6
lhmoreno <lhmorenoti@hotmail.com>
2021-11-29 13:49:09 -0300

    feat: Create view chat

--- server/src/controllers/ChatController.ts
+++ server/src/controllers/ChatController.ts
@@ -3,26 +3,29 @@ import { Request, Response } from 'express'
 import { utils } from '../validators/utils'
 import { UserService } from '../services/UserService'
 import { ChatService } from '../services/ChatService'
+import { ChatView } from '../views/ChatView'
 
 async function createMessage(req: Request, res: Response) {
   const token = req.headers.authorization
   const { user_id_2 } = req.params
-  const { message } = req.body
+  const { message: message_send } = req.body
 
   // Validators
   if (typeof(token) !== 'string') return res.status(401).json({ error: 'Token is a requirement' })
-  if (typeof(message) !== 'string') return res.status(400).json({ error: 'Invalid message' })
+  if (typeof(message_send) !== 'string') return res.status(400).json({ error: 'Invalid message' })
 
   try {
     // Services
     const user_id_1 = await UserService.findUserIdByToken(token.split(' ')[1])
     const newMessage = await ChatService.createMessageByUsersIds(user_id_1, user_id_2, {
       user_id: user_id_1,
-      text: message,
+      text: message_send,
       hour: '12:37pm'
     })
 
-    return res.status(201).json({ message: newMessage })
+    const message = ChatView.renderMessage(newMessage)
+
+    return res.status(201).json({ message })
   } catch (err) {
     if (utils.isServiceError(err)) {
       err.message_server && console.log(err.message_server)
@@ -43,9 +46,11 @@ async function indexMessages(req: Request, res: Response) {
   try {
     // Services
     const user_id_1 = await UserService.findUserIdByToken(token.split(' ')[1])
-    const messages = await ChatService.findMessagesByUsersIds(user_id_1, user_id_2)
+    const messages_db = await ChatService.findMessagesByUsersIds(user_id_1, user_id_2)
 
-    return res.status(201).json({ messages })
+    const messages = ChatView.renderManyMessages(messages_db)
+
+    return res.status(200).json({ messages })
   } catch (err) {
     if (utils.isServiceError(err)) {
       err.message_server && console.log(err.message_server)
---
+++ server/src/views/ChatView.ts
@@ -0,0 +1,17 @@
+import { Message } from '../..'
+
+function renderMessage(message: Message) {
+  return {
+    message_id: message._id,
+    user_id: message.user_id,
+    text: message.text,
+    hour: message.hour,
+    status: message.status
+  }
+}
+
+function renderManyMessages(chat: Message[]) {
+  return chat.map((message) => renderMessage(message))
+}
+
+export const ChatView = { renderMessage, renderManyMessages }
